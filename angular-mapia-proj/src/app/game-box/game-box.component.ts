31ef5d2f1b05ecea111423ddd939515f9a945728
Taylor <rkdxogml5768@naver.com>
2017-12-22 23:08:14 +0900

    score fail update

--- angular-mapia-proj/src/app/game-box/game-box.component.ts
+++ angular-mapia-proj/src/app/game-box/game-box.component.ts
@@ -22,29 +22,27 @@ declare var $:any
 
 export class GameBoxComponent implements OnInit {
   
-  score = new Score();
-  
-  wordFromUser="";
+  score = new Score();  //user Score
+  wordFromUser="";  //user Input word
 
   getInput(event: String) {
     this.wordFromUser = event.toString();
     console.log(this.wordFromUser);
   }
 
-  words = WORDS;
-  fallWords = FALLING_WORDS;
+  words = WORDS;  //word List
+  fallWords = FALLING_WORDS;  //Falling Word List
 
   subForAdd: Subscription;
   subForFall: Subscription;
-  
   timerForAdd;
   timerForFall;
 
   constructor() { }
 
-
   len = this.words.length;
 
+  //5초마다 떨어지는 단어 추가 
   addWordInint(){
     var i = 0;
     this.timerForAdd = Observable.timer(0,5000);
@@ -53,40 +51,56 @@ export class GameBoxComponent implements OnInit {
       if(i < this.len){
         this.fallWords.push(this.words[i]);
         this.fallWords[i].y = Math.floor(Math.random() * 80);
-        this.fallWords[i].x = Math.floor(Math.random() * 1000);
+        this.fallWords[i].x = Math.floor(Math.random() * 800 + 100);
         i++;
 
       }
     });
   }
 
+  //0.5초마다 밑으로 떨어지는 단어
   dropInit(){
 
-    this.timerForFall = Observable.timer(0,500);
+    this.timerForFall = Observable.timer(0,300);
     this.subForFall = this.timerForFall.subscribe(t=> {
      
       for(var i in this.fallWords){
 
-        if(this.fallWords[i].name == this.wordFromUser){
+        //success
+        if(this.fallWords[i].name == this.wordFromUser
+          && $("." + this.fallWords[i].name).css("visibility") != "hidden"){
           this.score.success++;
+          this.wordFromUser = "";
           $("." + this.fallWords[i].name).css("visibility", "hidden");
         }
-        
-        this.fallWords[i].y += 10;
 
+        //drop the word
+        this.fallWords[i].y += 10;
         $("." + this.fallWords[i].name).css("top", this.fallWords[i].y + "px");
         $("." + this.fallWords[i].name).css("left", this.fallWords[i].x + "px");
+       
+        //alert
         if(this.fallWords[i].y >= 400){
           $("." + this.fallWords[i].name).css("color", "red");
           $("." + this.fallWords[i].name).css("background", "lightgrey");
         }
+
+        //fail
         if(this.fallWords[i].y >= 610){
-          if(this.fallWords[i].y >= 610 && this.fallWords[i].y <= 620){
+          if(this.fallWords[i].y >= 610 
+            && this.fallWords[i].y <= 620
+            && $("." + this.fallWords[i].name).css("visibility") != "hidden"){
             this.score.fail++;
+            
+            //game over
+            if(this.score.fail == 8){
+               alert("fail!");
+            }
+
           }
           $("." + this.fallWords[i].name).css("visibility", "hidden");
-
         }
+
       }
     });
   }
