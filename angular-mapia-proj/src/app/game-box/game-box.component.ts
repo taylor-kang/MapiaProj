fa7e54b27766a98e740d426815d2b70d8e5f6326
Taylor <rkdxogml5768@naver.com>
2017-12-27 22:32:01 +0900

    fix #5

--- angular-mapia-proj/src/app/game-box/game-box.component.ts
+++ angular-mapia-proj/src/app/game-box/game-box.component.ts
@@ -16,14 +16,9 @@ declare var $:any
 export class GameBoxComponent implements OnInit {
   
   score = new Score();  //user Score
-  wordFromUser="";  //user Input word
-
-  getInput(event: String) {
-    this.wordFromUser = event.toString();
-    console.log(this.wordFromUser);
-  }
-
+  
   words = WORDS;  //word List
+  len = this.words.length;
   fallWords = FALLING_WORDS;  //Falling Word List
 
   subForAdd: Subscription;
@@ -31,24 +26,30 @@ export class GameBoxComponent implements OnInit {
   timerForAdd;
   timerForFall;
   
+  //user Input word
+  wordFromUser="";  
+  getInput(event: String) {
+    this.wordFromUser = event.toString();
+    console.log(this.wordFromUser);
+  }
+
   constructor(private elementRef: ElementRef, public renderer: Renderer) { }
 
-  len = this.words.length;
-
+  
   //5초마다 떨어지는 단어 추가 
   addWordInint(){
     var i;
     this.timerForAdd = Observable.timer(0,5000);
-    
     this.subForAdd = this.timerForAdd.subscribe(t=> { 
       i = Math.floor(Math.random() * 1000) % this.len; 
       
-      this.fallWords.push(this.words[i]);
       console.log("add : " + this.words[i].name); 
       
-      this.fallWords[this.fallWords.length-1].y = Math.floor(Math.random() * 80);
-      this.fallWords[this.fallWords.length-1].x = Math.floor(Math.random() * 800 + 100);
-       
+      let tmp  = this.words[i];
+      tmp.y = Math.floor(Math.random() * 80);
+      tmp.x = Math.floor(Math.random() * 800 + 100);
+      this.fallWords.push(tmp);
+         
       console.log(this.fallWords);
     });
   }
@@ -59,26 +60,16 @@ export class GameBoxComponent implements OnInit {
     this.subForFall = this.timerForFall.subscribe(t=> {
 
       this.fallWords.forEach(fallingWord => { 
-        
-        
         if(fallingWord.life == 1){
           //success
           if(fallingWord.name == this.wordFromUser){
             console.log("success : " + fallingWord.name);
-            let selectedWord = this.elementRef.nativeElement.querySelector("." + fallingWord.name);          
-            this.renderer.setElementStyle(selectedWord, 'visibility', 'hidden');
             
             //delete from fallWord Array
-            //let tmp = new Word(fallingWord.name, fallingWord.x, fallingWord.y);
             let idx = this.fallWords.indexOf(fallingWord, 0);
             this.fallWords.splice(idx, 1);
             console.log(this.fallWords);
-            // fallingWord.x = 0;
-            // fallingWord.y = 0;
-            // fallingWord.life = 0;
-            // this.renderer.setElementStyle(selectedWord, 'top', fallingWord.y + "px");
-            // this.renderer.setElementStyle(selectedWord, 'left', fallingWord.x + "px");
-
+            
             this.score.success++;
             this.wordFromUser = "";
           }
@@ -86,12 +77,10 @@ export class GameBoxComponent implements OnInit {
           else {
             //drop
             let selectedWord = this.elementRef.nativeElement.querySelector("." + fallingWord.name);
-            
-            //drop the word
             fallingWord.y += 10;
             this.renderer.setElementStyle(selectedWord, 'top', fallingWord.y + "px");
             this.renderer.setElementStyle(selectedWord, 'left', fallingWord.x + "px");
-            
+    
             //alert
             if(fallingWord.y >= 400){
               this.renderer.setElementStyle(selectedWord, 'color', 'red');
@@ -101,27 +90,16 @@ export class GameBoxComponent implements OnInit {
             //fail
             if(fallingWord.y >= 610){
               console.log("fail : " + fallingWord.name);
+              
               //delete from fallWord Array
               this.renderer.setElementStyle(selectedWord, 'visibility', 'hidden');
-              
-              //let tmp = new Word(fallingWord.name, fallingWord.x, fallingWord.y);
               let idx = this.fallWords.indexOf(fallingWord, 0);
               this.fallWords.splice(idx, 1);
               console.log(this.fallWords);
 
               this.score.fail++;
-              // fallingWord.x = 0;
-              // fallingWord.y = 0;
-              //fallingWord.life = 0;
-              //this.renderer.setElementStyle(selectedWord, 'top', fallingWord.y + "px");
-              //this.renderer.setElementStyle(selectedWord, 'left', fallingWord.x + "px");
-
               //game over
-              if(this.score.fail == 8){
-                 return;
-              }
-              
-
+              if(this.score.fail == 8){ return; }
             }
           }
         }
